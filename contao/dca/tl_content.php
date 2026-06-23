b2ffd42c107362ae781d7350fac611c19bdcee66
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-06-23 10:12:47 +0200

    Use virtual fields instead

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -4,16 +4,10 @@ declare(strict_types=1);
 
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
-/*
- * A checkbox reveals the ratio field (sub-palette); both are added to the core
- * "element_group" content element and stored as real columns.
- */
-
 $GLOBALS['TL_DCA']['tl_content']['fields']['gridRatioActive'] = [
     'exclude' => true,
     'inputType' => 'checkbox',
     'eval' => ['tl_class' => 'w50 clr', 'submitOnChange' => true],
-    'sql' => ['type' => 'boolean', 'default' => false],
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['gridRatio'] = [
@@ -21,13 +15,11 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['gridRatio'] = [
     'exclude' => true,
     'inputType' => 'gridRatio',
     'eval' => ['tl_class' => 'clr'],
-    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['__selector__'][] = 'gridRatioActive';
 $GLOBALS['TL_DCA']['tl_content']['subpalettes']['gridRatioActive'] = 'gridRatio';
 
-// addLegend is idempotent: an existing "grid_legend" is reused, otherwise created.
 PaletteManipulator::create()
     ->addLegend('grid_legend', 'type_legend', PaletteManipulator::POSITION_AFTER)
     ->addField('gridRatioActive', 'grid_legend', PaletteManipulator::POSITION_APPEND)
